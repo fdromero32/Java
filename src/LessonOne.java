f82b89a822881c3c76a19b230efd4368b27851e0
Danny Romero <fdromero32@gmail.com>
2021-01-19 16:56:22 -0800

    Learning about println() and printf() method and how to format

---
+++ src/LessonOne.java
@@ -0,0 +1,8 @@
+public class LessonOne {
+    public static void main(String[] args) {
+        System.out.println(3*4);
+        System.out.printf("5 * 2 = %d", 5*2).println();
+        System.out.printf("%d * %d = %d", 5, 7, 5*7).println();
+        System.out.printf("%d + %d + %d = %d", 5, 6, 7, 5+6+7).println();
+    }
+}
