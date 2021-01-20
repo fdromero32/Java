660e3a102ce88472e9e51bf6a82d97aedc3dd6e0
Danny Romero <fdromero32@gmail.com>
2021-01-19 17:20:36 -0800

    variable exercises in Java - beginner

--- src/LessonOne.java
+++ src/LessonOne.java
@@ -1,8 +1,22 @@
 public class LessonOne {
     public static void main(String[] args) {
-        System.out.println(3*4);
-        System.out.printf("5 * 2 = %d", 5*2).println();
-        System.out.printf("%d * %d = %d", 5, 7, 5*7).println();
-        System.out.printf("%d + %d + %d = %d", 5, 6, 7, 5+6+7).println();
+//        System.out.println(3*4);
+//        System.out.printf("5 * 2 = %d", 5*2).println();
+//        System.out.printf("%d * %d = %d", 5, 7, 5*7).println();
+//        System.out.printf("%d + %d + %d = %d", 5, 6, 7, 5+6+7).println();
+        int number = 10;
+        number = 11;
+        number = 34;
+        System.out.println(number);
+
+        int i = 15;
+        System.out.printf("%d * %d  = %d", 5, i, 5*i).println();
+
+        //first variable exercise with printf()
+        int a = 10;
+        int b = 50;
+        int c = 93;
+        System.out.printf("%d + %d + %d = %d", a, b, c, a+b+c).println();
+
     }
 }
